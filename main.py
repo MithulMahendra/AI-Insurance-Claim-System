1f0176d8c847c17d9e2c700f4782b32e65885465
Mithul Mahendra M <119515513+MithulMahendra@users.noreply.github.com>
2026-04-01 21:34:18 +0530

    Initialize FastAPI application with routing
    Set up FastAPI with routers for query and admin endpoints.

--- main.py
+++ main.py
@@ -1,6 +1,14 @@
-def main():
-    print("Hello from ai-insurance-claim-system!")
+from fastapi import FastAPI
+from src.api.v1.routes.query import router as query_router
+from src.api.v1.routes.admin import router as admin_router
 
-#Parasuram commit
-if __name__ == "__main__":
-    main()
+
+app = FastAPI(
+    title="Policy RAG API",
+    description="Hybrid RAG system for Insurance policy document Q&A",
+    version="1.0.0",
+)
+
+#Prefixes
+app.include_router(query_router, prefix="/api/v1")
+app.include_router(admin_router, prefix="/api/v1/admin")
